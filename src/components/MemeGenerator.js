cf09ffeb618f026c16743f191150f613135b9157
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-15 20:40:04 +0900

    template set

---
+++ src/components/MemeGenerator.js
@@ -0,0 +1,19 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+class MemeGenerator extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <h1>MEME GENERATOR</h1>
+    )
+  }
+}
+
+export default MemeGenerator
